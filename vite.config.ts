1835484e03f6d00d34f5dd39c9d4ec07211053f5
unknown <you@example.com>
2024-10-22 14:41:20 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: globalThis.process.env.NODE_ENV === "production" ? "/todo-react/" : "/",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: `${command === "serve" ? "/" : "/todo-react/"}`,
+  };
+
+  // if (command !== "serve") {
+  //   config.base = "/todo-react/";
+  // }
+
+  return config;
 });
